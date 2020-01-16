7efa936bf0df47810d23fede3698413395123dfb
moustaxx <maciasm1303@gmail.com>
2020-01-16 15:34:24 +0100

    Repo init

---
+++ src/app/main-view/main-view.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './main-view.component.html',
+  styleUrls: ['./main-view.component.scss']
+})
+export class MainViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
