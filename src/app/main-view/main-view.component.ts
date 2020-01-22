e323d484caff024cf9ebac12caded6a26e013c5b
moustaxx <maciasm1303@gmail.com>
2020-01-22 18:15:10 +0100

    Partially implement math operation handling

--- src/app/main-view/main-view.component.ts
+++ src/app/main-view/main-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 const buttonsContent = [
   1, 2, 3, '÷', 'pow',
@@ -12,11 +12,76 @@ type TButtonItemContent = typeof buttonsContent[number];
   templateUrl: './main-view.component.html',
   styleUrls: ['./main-view.component.scss']
 })
+export class MainViewComponent {
   buttonsContent = buttonsContent;
+  mathOperation: string[] = [];
 
-  constructor() { }
+  undoLastOperation() {
+    const poppedMathOperation = this.mathOperation.slice(0, -1);
+    this.mathOperation = poppedMathOperation;
+  }
 
-  ngOnInit() {
+  clearMathOperations() {
+    this.mathOperation = [];
+  }
+
+  getResult() {
+
+  }
+
+  getLastOperation() {
+    return this.mathOperation[this.mathOperation.length - 1] || null;
+  }
+
+  concatLastOperation(operation: number) {
+    const lastOp = this.getLastOperation() || '';
+    if (!Number(lastOp)) {
+      this.addOperation(String(operation));
+      return;
+    }
+    const parsedOperation = operation;
+    const concatedOp = `${lastOp}${parsedOperation}`;
+    const opsWithoutLast = this.mathOperation.slice(0, -1);
+
+    this.mathOperation = [...opsWithoutLast, concatedOp];
+  }
+
+  addOperation(op: string) {
+    this.mathOperation = [...(this.mathOperation || []), op];
+  }
+
+  handleDot() {
+    const lastOp = this.getLastOperation();
+    if (lastOp?.includes('.') || !Number(lastOp)) return;
+    const concatedOp = `${lastOp}.`;
+    const opsWithoutLast = this.mathOperation.slice(0, -1);
+
+    this.mathOperation = [...opsWithoutLast, concatedOp];
+  }
+
+  dispatchAction(event: TButtonItemContent) {
+    switch (event) {
+      case 'C':
+        this.undoLastOperation();
+        break;
+      case 'CE':
+        this.clearMathOperations();
+        break;
+      case '=':
+        this.getResult();
+        break;
+      case '.':
+        this.handleDot();
+        break;
+      default:
+        if (typeof event === 'number') {
+          this.concatLastOperation(event);
+          return;
+        }
+
+        this.addOperation(event);
+        break;
+    }
   }
 
 }
